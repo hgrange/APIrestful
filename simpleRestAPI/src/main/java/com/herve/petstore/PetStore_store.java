30ecd8fc5078ba8e67184abbb5b6c5c94478dfa8
hgrange <33756056+hgrange@users.noreply.github.com>
2024-10-14 17:24:08 +0200

    Add files via upload

---
+++ simpleRestAPI/src/main/java/com/herve/petstore/PetStore_store.java
@@ -0,0 +1,110 @@
+// tag::copyright[]
+/*******************************************************************************
+ * Copyright (c) 2017, 2022 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+// end::copyright[]
+package com.herve.petstore;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+import jakarta.json.Json;
+import jakarta.json.JsonArray;
+import jakarta.json.JsonObject;
+import jakarta.json.JsonReader;
+import jakarta.json.stream.JsonParser;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DefaultValue;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.HttpHeaders;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.MultivaluedMap;
+import jakarta.ws.rs.core.UriInfo;
+
+@Path("store")
+public class PetStore_store {
+
+ 
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("inventory")
+
+    public JsonObject findByStatus(@DefaultValue("sold") @QueryParam("status") String status) {
+    	    JsonObject jsonObj = null;
+    	    String fileName = "PetStoreInventoryStore.json"; ;
+    	       	      	    
+    		ClassLoader myclass = getClass().getClassLoader();
+			URL resource = getClass().getClassLoader().getResource(fileName);
+			System.out.println("URL resource = " + resource);
+			String sFile = resource.toString().replaceFirst("wsjar:", "");
+			System.out.println("String sFile = "+ sFile );
+			URI uri;
+			try {
+				uri = new URI(sFile);
+	
+
+				System.out.println("uri = " + uri.toString());
+				java.nio.file.Path path = Paths.get(uri);
+				System.out.println("java.nio.file.Path path = " + path.toString());
+				String s_path = Files.readString(path);
+				
+			    
+				StringReader sr = new StringReader( s_path);
+				
+			
+				JsonReader jsonReader = Json.createReader(sr);
+			
+				//JsonReader jsonReader = Json.createReader(new StringReader(Files.readString(Paths.get(resource.toURI()))));
+			    jsonObj = jsonReader.readObject();
+            } catch (URISyntaxException e) {
+		       e.printStackTrace();
+    } catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+     	return jsonObj;
+   }
+    
+    @POST
+    @Path("{var:.+}")
+    @Produces(MediaType.APPLICATION_JSON)
+
+    public String post(@Context UriInfo ui, @PathParam("var") String path, @Context HttpHeaders headers) {
+        MultivaluedMap<String, String> queryParams = ui.getQueryParameters();
+        MultivaluedMap<String, String> pathParams = ui.getPathParameters();
+        MultivaluedMap<String, String> headersParams = headers.getRequestHeaders();
+    	String response = "{ \"data\":1919 }";
+        return response;
+    }
+    
+    @PUT
+    @Path("{var:.+}")
+    @Produces(MediaType.APPLICATION_JSON)
+
+    public String put(@Context UriInfo ui, @PathParam("var") String path, @Context HttpHeaders headers) {
+        MultivaluedMap<String, String> queryParams = ui.getQueryParameters();
+        MultivaluedMap<String, String> pathParams = ui.getPathParameters();
+        MultivaluedMap<String, String> headersParams = headers.getRequestHeaders();
+    	String response = "{ \"data\":1919 }";
+        return response;
+    }
+}
