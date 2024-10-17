85456f466f575741013b532d16e07135c7612048
hgrange <33756056+hgrange@users.noreply.github.com>
2024-10-17 18:08:09 +0200

    Add files via upload

--- simpleRestAPI/src/main/java/com/herve/petstore/UserDao.java
+++ simpleRestAPI/src/main/java/com/herve/petstore/UserDao.java
@@ -18,7 +18,9 @@ public class UserDao {
     }
 
     public User readUser(String username) {
-        return em.find(User.class, username);
+    	User user = em.find(User.class, username);
+    	user.setPassword("*******");
+        return user;
     }
 
     public void updateUser(User user) {
@@ -30,12 +32,21 @@ public class UserDao {
     }
 
     public List<User> readAllUsers() {
-        return em.createNamedQuery("User.findAll", User.class).getResultList();
+        List<User> lUsers= em.createNamedQuery("User.findAll", User.class).getResultList();
+ /*       for (User user : lUsers ) {
+        	user.setPassword("*******");
+        } */
+    	return lUsers;
     }
+    
 
     public List<User> findUser(String username) {
-        return em.createNamedQuery("User.findUser", User.class)
+        List<User> lUsers= em.createNamedQuery("User.findUser", User.class)
             .setParameter("username", username).getResultList();
+  /*      for (User user : lUsers ) {
+        	user.setPassword("*******");
+        }*/
+        return lUsers;
 
     }
 }
