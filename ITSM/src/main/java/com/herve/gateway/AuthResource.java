24fa0c336ad28d9fb3e502f6688ba22447bca932
hgrange <33756056+hgrange@users.noreply.github.com>
2025-07-30 15:05:34 +0200

    Update AuthResource.java

--- ITSM/src/main/java/com/herve/gateway/AuthResource.java
+++ ITSM/src/main/java/com/herve/gateway/AuthResource.java
@@ -74,6 +74,8 @@ public class AuthResource extends HttpServlet {
 
     String url = request.getRequestURL().toString();
     LOGGER.info("request URL: " + url);
+    String serverName = request.getServerName();
+    LOGGER.info("Server Name: " + serverName);
     String redirect_url = url;
     if (!url.endsWith("v2/cmdbs")) {
       if (!url.endsWith("v2/incidents")) {
@@ -81,7 +83,8 @@ public class AuthResource extends HttpServlet {
           if (!url.endsWith("incident.xhtml")) {
             if (!url.endsWith("favicon.ico")) {
               LOGGER.info("URL does not end with v2/cmdb, v2/incidents, cmdb.xhtml, favicon.io or incident.xhtml");
-              redirect_url = url + "/incident.xhtml";
+              //redirect_url = url + "/incident.xhtml";
+              redirect_url = "https://"+serverName + "/incident.xhtml";
             }
           }
         }
