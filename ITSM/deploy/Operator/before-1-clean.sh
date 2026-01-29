035e37cc58c179408afd80a3b2c60e931ffcd350
hgrange <33756056+hgrange@users.noreply.github.com>
2026-01-29 10:00:43 +0100

    Update before-1-clean.sh

--- ITSM/deploy/Operator/before-1-clean.sh
+++ ITSM/deploy/Operator/before-1-clean.sh
@@ -1,4 +1,4 @@
 oc delete webspherelibertyapplication itsm -n NAMESPACE
 oc delete openlibertyapplication -n NAMESPACE
-
+oc create secret itsm-oidc --from-literal=oidc.properties="issuerUri=$(
 # oc delete pvc itsm-db -n NAMESPACE
